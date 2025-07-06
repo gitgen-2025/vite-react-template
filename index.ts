32997492cf0de06f5a8a837b38b01ce30c42261d
gitgen-2025 <genevamcbhy@gmailcom>
2025-07-06 17:51:33 +0000

    Parameter edit

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ type Bindings = {
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get('/video/:filename', async (c) => {
-  const filename = c.req.param('GNVA.mp4');
-  const object = await c.env.VIDEO_BUCKET.get(https)//pub-8dfa8cff12d8425e80230d8ebde72d6f.r2.dev/GNVA.mp4);
+  const filename = c.req.param('filename');
+  const object = await c.env.VIDEO_BUCKET.get(https://pub-8dfa8cff12d8425e80230d8ebde72d6f.r2.dev/GNVA.mp4);
 
   if (!object || !object.body) {
     return new Response("File not found", { status: 404 });
